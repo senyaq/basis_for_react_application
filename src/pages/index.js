947be30f13fa6921bb65a420fe53a28a211da39c
senyaq <senyaq@gmail.com>
2019-09-18 10:07:07 +0300

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ function Home() {
 
   return (
     <Fragment>
-      <h1>Home</h1>
+      <h1>Home2</h1>
       {posts.length < 1 && <h3>Loading ...</h3>}
       <ul>{posts.length > 0 && posts.map(item => <li key={`${item.id}`}>{item.title}</li>)}</ul>
     </Fragment>
